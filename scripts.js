3aecb55345da9656f9af655e1d77130c1009578e
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-03-30 09:54:05 -0300

    Adicionar elementos a um objeto em JavaScript

---
+++ scripts.js
@@ -0,0 +1,51 @@
+// =
+const a = 1;
+
+const person = {};
+person.name = "Dimas";
+person.age = 40;
+
+console.log(person);
+
+// []
+const car = {
+  rodas: 4,
+  portas: 4,
+};
+
+car["cor"] = "Vermelho";
+
+console.log(car);
+
+const nomePropriedade = "portas";
+car[nomePropriedade] = 2;
+
+console.log(car);
+console.log(nomePropriedade);
+
+// Object => defineProperty
+Object.defineProperty(car, "motor", {
+  value: 1.4,
+  writable: false, //não deixa subistituir o valor
+});
+console.log(car);
+
+// defineProperties
+const pessoa = {};
+
+Object.defineProperties(pessoa, {
+  nome: {
+    value: "Mariana",
+    writable: false,
+  },
+  idade: {
+    value: 37,
+    writable: true,
+  },
+  profissao: {
+    value: "Programadora",
+    writable: false,
+  },
+});
+
+console.log(pessoa);
